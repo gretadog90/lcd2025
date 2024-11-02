aa1b9c2c89b6acbcb70305a375bee2f19cfa35f2
greta_lap <gretam@gwu.edu>
2024-11-02 15:56:02 -0400

    draft through results with new graph code

---
+++ paper code/8 Fig 5 hia results violin.R
@@ -0,0 +1,77 @@
+#########################################################################
+# Figure 5: HIA 2014-2018 v 2019-2023 by region and climate zone
+#########################################################################
+#### Set up ####
+#clear objects
+rm(list = ls()) 
+
+#libraries
+library(tidyr)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(tidytab)
+library(ggpubr)
+library(egg)
+library(ggridges)
+
+####import####
+#set working directory
+setwd('~/Documents/data/Lancet 2025/')
+
+#read in HIA results
+hia<-read.csv("outputHIA/hia_100m_5yr.csv")
+hia<-hia[!is.na(hia$ndvi2019_2023),]
+hist(hia$delta_mortality)
+
+hia$diff<-hia$ndvi2019_2023-hia$ndvi2014_2018
+hist(hia$diff)
+
+hia$delta_mortality<-(hia$delta_mortality/hia$Population_2020_100m)*100000
+summary(hia$delta_mortality)
+hist(hia$delta_mortality)
+
+hia<- hia %>%
+  group_by(sub.region) %>% 
+  mutate(order = (cur_group_id()-15)*-1)
+
+nopolar<-subset(hia, clim_region!="Polar")
+
+nopolar<- nopolar %>%
+  group_by(clim_region) %>% 
+  mutate(order = (cur_group_id()-4)*-1)
+
+a<-ggplot(data=hia, aes(x=delta_mortality, y=reorder(sub.region, order), fill=sub.region,)) +
+  geom_violin(width=2.1, size=0.2) +
+  xlab("")+
+  ylab("")+
+  labs(fill="Geographic Region")+
+  scale_x_continuous(breaks=c(-500,-250,0, 250, 500),
+                     labels=c("500 more", "250 more", "0", "250 fewer", "500 fewer"))+
+  theme(legend.position = "none",
+        axis.text=element_text(size=9),
+        axis.title=element_text(size=11))
+
+b<-ggplot(data=nopolar, aes(x=delta_mortality, y=reorder(clim_region, order), fill=clim_region,)) +
+  geom_violin(width=2.1, size=0.2) +
+  ylab("")+
+  xlab("Attributable deaths per 100,000 from changes in NDVI")+
+  labs(fill="Köppen-Geiger\nclimate classification")+
+  scale_fill_brewer(palette = "PuOr")+
+  scale_x_continuous(breaks=c(-500,-250,0, 250, 500),
+                     labels=c("500 more", "250 more", "0", "250 fewer", "500 fewer"))+
+  theme(legend.position = "none",
+        axis.text=element_text(size=9),
+        axis.title=element_text(size=11))
+
+#set up the file to save figure
+pdf(file ="graphs/Fig 5 mort by region and climate violin.pdf")
+
+figure <- ggarrange(a, b,
+                    widths=11, heights=c(5,5),
+                    labels = c("A", "B"),
+                    ncol = 1, nrow = 2)
+
+dev.off()
---
+++ paper code/archive/Fig 2 diff and hia by region.R
@@ -0,0 +1,78 @@
+#########################################################################
+# Figure 3: % Exposure by sub.region (diff and pct diff)
+#########################################################################
+#### Set up ####
+#clear objects
+rm(list = ls()) 
+
+#libraries
+library(tidyr)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(tidytab)
+library(ggpubr)
+library(egg)
+
+####import####
+#set working directory
+setwd('~/Documents/data/Lancet 2025/')
+
+#merged exposure data
+hia<-read.csv("outputHIA/hia_100m_5yr.csv")
+hia<-hia[!is.na(hia$ndvi2019_2023),]
+
+hia$diff<-hia$ndvi2019_2023-hia$ndvi2014_2018
+hist(hia$diff)
+hia$pct_diff<-((hia$ndvi2019_2023-hia$ndvi2014_2018)/hia$ndvi2014_2018)*100
+hist(hia$pct_diff)
+
+hia$delta_mortality<-(hia$delta_mortality/hia$Population_2020_100m)*100000
+summary(hia$delta_mortality)
+hist(hia$delta_mortality)
+
+#make boxplots for NDVI by region
+a<-ggplot(hia, aes(x=sub.region, y=pct_diff, color=sub.region)) + 
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitter(0.2), size=.6)+
+  xlab("")+ 
+  ylab("Percent change in NDVI 2014-2018 v. 2019-2023")+
+  ylim(-25, 30)+
+  coord_flip()+
+  theme(legend.position="none")
+  
+b<-ggplot(data=hia, aes(x=pct_diff, group=sub.region, fill=sub.region)) +
+  geom_density(alpha=.4)+xlab("")+
+  ylab("Density")+
+  scale_x_continuous(breaks=c(-20,-10,0, 20, 20),
+                     labels=c("-20%", "-10%", "0", "+10%", "+20%"))+
+  theme(legend.position="none")
+
+c<-boxplot2023<-ggplot(hia, aes(x=sub.region, y=delta_mortality, color=sub.region)) + 
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(position=position_jitter(0.2), size=.6)+
+  xlab("")+ 
+  ylab("    Associated changes in deaths per 100,000 from NDVI changes 2014-2018 v. 2019-2023")+
+  ylim(-600, 600)+
+  coord_flip()+
+  theme(legend.position="none")
+  
+d<-ggplot(data=hia, aes(x=delta_mortality, group=sub.region, fill=sub.region)) +
+  geom_density(alpha=.4)+xlab("")+
+  ylab("Density")+labs(fill="Geographic region")+
+  scale_x_continuous(breaks=c(-600,-300,0, 300, 600),
+                     labels=c("+600 deaths", "+300 deaths", "0", "-300 deaths", "-600 deaths"))+
+  theme(legend.position="none")
+
+
+#set up the file to save figure
+pdf(file = "graphs/subregion_panels.pdf")
+
+figure <- ggarrange(a, b, c, d,
+                    widths=c(8,8), heights=c(5,5),
+                    labels = c("A", "B", "C", "D"),
+                    ncol = 2, nrow = 2)
+
+dev.off()
